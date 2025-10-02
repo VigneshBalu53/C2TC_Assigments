64ec7d276c60b21a6bd62fbd730014bb0628836e
vigne <vigne@Vignesh>
2025-10-02 19:08:16 +0530

    Assignment1 completed

---
+++ assignment/src/com/vignesh/assignment1/AssignmentMain.java
@@ -0,0 +1,26 @@
+package com.vignesh.assignment1;
+import com.vignesh.assignment1.employees.Developer;
+import com.vignesh.assignment1.employees.Manager;
+import com.vignesh.assignment1.utilities.EmployeeUtilities;
+
+// Main class to demonstrate package usage and access modifiers.
+public class AssignmentMain {
+    public static void main(String[] args) {
+        // Creating Manager Object
+        Manager manager = new Manager("Arun", 101, 120000, "HR");
+        
+        // Creating Developer Objects
+        Developer developer = new Developer("Kumar", 102, 80000, "Java");
+
+        // Creating Utility Object
+        EmployeeUtilities util = new EmployeeUtilities();
+
+        // Display Details
+        util.printEmployeeDetails(manager);
+        util.printEmployeeDetails(developer);
+
+        // Update Salary Using Protected Method
+        util.updateSalary(manager, 95000);
+        util.updateSalary(developer, 80000);
+    }
+}
